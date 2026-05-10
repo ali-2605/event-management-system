9e8644404b0377cbd39231aa6fc5860a45821a6d
Mohammed Hamdy <142733380+Mohame-Hamdy@users.noreply.github.com>
2026-05-10 07:30:27 +0300

    Clean up auth_provider.g.dart
    cleaning

--- lib/features/auth/auth_provider.g.dart
+++ lib/features/auth/auth_provider.g.dart
@@ -1,13 +1,7 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
 
 part of 'auth_provider.dart';
 
-// **************************************************************************
-// RiverpodGenerator
-// **************************************************************************
 
-// GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: type=lint, type=warning
 
 @ProviderFor(Auth)
 final authProvider = AuthProvider._();
