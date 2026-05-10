da7a5c27c8d0934bc6832c8f52d2513cfd3c299c
Mohammed Hamdy <142733380+Mohame-Hamdy@users.noreply.github.com>
2026-05-10 07:22:48 +0300

    Cleaned up  registration_provider.g.dart
    cleaning

--- lib/features/registrations/registration_provider.g.dart
+++ lib/features/registrations/registration_provider.g.dart
@@ -1,13 +1,7 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
 
 part of 'registration_provider.dart';
 
-// **************************************************************************
-// RiverpodGenerator
-// **************************************************************************
 
-// GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: type=lint, type=warning
 
 @ProviderFor(Registrations)
 final registrationsProvider = RegistrationsProvider._();
