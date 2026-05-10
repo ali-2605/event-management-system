4334796aad5c7af06b70b2e56c536fc89ae52a76
Mohammed Hamdy <142733380+Mohame-Hamdy@users.noreply.github.com>
2026-05-10 07:25:26 +0300

    Clean up notification_provider.g.dart
    cleaning

--- lib/features/notifications/notification_provider.g.dart
+++ lib/features/notifications/notification_provider.g.dart
@@ -1,13 +1,7 @@
-// GENERATED CODE - DO NOT MODIFY BY HAND
 
 part of 'notification_provider.dart';
 
-// **************************************************************************
-// RiverpodGenerator
-// **************************************************************************
 
-// GENERATED CODE - DO NOT MODIFY BY HAND
-// ignore_for_file: type=lint, type=warning
 
 @ProviderFor(Notifications)
 final notificationsProvider = NotificationsProvider._();
